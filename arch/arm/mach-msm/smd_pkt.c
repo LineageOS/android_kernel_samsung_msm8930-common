50578b3305fbd9f7f7ceb65a5da4de6bc2a86d51
Eric Holmberg <eholmber@codeaurora.org>
2013-04-16 18:22:44 -0600

    msm: smd_pkt: Add APR channel for testing
    Add "apr" channel used for testing of audio functionality.
    
    Change-Id: I5a5e53bc270ed141743edfe7d519f12f094a2fea
    Signed-off-by: Eric Holmberg <eholmber@codeaurora.org>

--- arch/arm/mach-msm/smd_pkt.c
+++ arch/arm/mach-msm/smd_pkt.c
@@ -41,7 +41,7 @@
 #ifdef CONFIG_ARCH_FSM9XXX
 #define NUM_SMD_PKT_PORTS 4
 #else
-#define NUM_SMD_PKT_PORTS 27
+#define NUM_SMD_PKT_PORTS 28
 #endif
 
 #define PDRIVER_NAME_MAX_SIZE 32
@@ -731,6 +731,7 @@ static char *smd_pkt_dev_name[] = {
 	"smd_test_framework",
 	"smd_logging_0",
 	"smd_data_0",
+	"apr",
 	"smd_pkt_loopback",
 };
 
@@ -761,6 +762,7 @@ static char *smd_ch_name[] = {
 	"TESTFRAMEWORK",
 	"LOGGING",
 	"DATA",
+	"apr",
 	"LOOPBACK",
 };
 
@@ -788,6 +790,7 @@ static uint32_t smd_ch_edge[] = {
 	SMD_APPS_MODEM,
 	SMD_APPS_QDSP,
 	SMD_APPS_WCNSS,
+	SMD_APPS_QDSP,
 	SMD_APPS_QDSP,
 	SMD_APPS_QDSP,
 	SMD_APPS_QDSP,
