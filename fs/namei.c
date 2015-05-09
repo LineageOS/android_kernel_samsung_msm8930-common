604c8f14b07163254c58610313bdd56951845c0b
Al Viro <viro@zeniv.linux.org.uk>
2015-05-08 22:53:15 -0400

    path_openat(): fix double fput()
    [ Upstream commit f15133df088ecadd141ea1907f2c96df67c729f0 ]
    
    path_openat() jumps to the wrong place after do_tmpfile() - it has
    already done path_cleanup() (as part of path_lookupat() called by
    do_tmpfile()), so doing that again can lead to double fput().
    
    Change-Id: I83bb7f0a15db8d2202a010b75ade98f80e7270f2
    Cc: stable@vger.kernel.org	# v3.11+
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- fs/namei.c
+++ fs/namei.c
@@ -2545,7 +2545,7 @@ static struct file *path_openat(int dfd, const char *pathname,
 
 	if (unlikely(filp->f_flags & __O_TMPFILE)) {
 		error = do_tmpfile(dfd, pathname, nd, flags, op, filp);
-		goto out;
+		goto out2;
 	}
 
 	error = path_init(dfd, pathname, flags | LOOKUP_PARENT, nd, &base);
@@ -2581,6 +2581,7 @@ out:
 		path_put(&nd->root);
 	if (base)
 		fput(base);
+out2:
 	release_open_intent(nd);
 	return filp;
 
