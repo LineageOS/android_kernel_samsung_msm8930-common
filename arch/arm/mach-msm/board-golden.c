a924c3551385ebc1e36f7e3a5b8814a48daa7b87
Arne Coucheron <arco68@gmail.com>
2020-10-01 01:07:10 +0200

    usb: gadget: Deprecate Samsung composite changes
    Change-Id: If9ba95f2c71be559eb1c1ea5776a3f9d5557e3b9

--- arch/arm/mach-msm/board-golden.c
+++ arch/arm/mach-msm/board-golden.c
@@ -2521,7 +2521,6 @@ static struct android_usb_platform_data android_usb_pdata = {
 #else
 	.cdrom = false,
 #endif
-	.nluns = 0,
 };
 
 static struct platform_device android_usb_device = {
--- drivers/usb/core/driver.c
+++ drivers/usb/core/driver.c
@@ -1764,9 +1764,7 @@ static int autosuspend_check(struct usb_device *udev)
 		}
 	}
 	if (w && !device_can_wakeup(&udev->dev)) {
-#ifndef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
 		dev_dbg(&udev->dev, "remote wakeup needed for autosuspend\n");
-#endif
 		return -EOPNOTSUPP;
 	}
 
